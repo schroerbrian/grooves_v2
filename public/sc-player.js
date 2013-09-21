ca6e36fb0a2988c6bbcd42fe97b80ea23006682a
Brian Schroer <schroer.brian@gmail.com>
2013-09-21 11:28:45 -0700

    cooking w gas. time to get a move on

--- public/sc-player.js
+++ public/sc-player.js
@@ -505,7 +505,7 @@
             .find('.sc-scrubber')
               .append('<div class="sc-volume-slider"><span class="sc-volume-status" style="width:' + soundVolume +'%"></span></div>')
               .append('<div class="sc-time-span"><div class="sc-waveform-container"></div><div class="sc-buffer"></div><div class="sc-played"></div></div>')
-              .append('<div class="sc-time-indicators"><span class="sc-position"></span> | <span class="sc-duration"></span></div>')
+              .append('<div class="sc-time-indicators sc-time-position-div"><span class="sc-position"></span><span class="sc-duration"></span></div>')
               .append('<div class="sc-controls"><a href="#prev" class="sc-prev">Prev</a><a href="#play" class="sc-play">Play</a> <a href="#pause" class="sc-pause hidden">Pause</a><a href="#next" class="sc-next">Next</a></div>');
 
         // load and parse the track data from SoundCloud API
