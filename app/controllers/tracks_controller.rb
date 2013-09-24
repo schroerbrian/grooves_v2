6dad459daef424f909bbf64c69af121151597fbc
Brian Schroer <schroer.brian@gmail.com>
2013-09-24 14:58:48 -0700

    Refine query method

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -10,8 +10,10 @@ class TracksController < ApplicationController
 
   #ajax call; used in test.html.erb script 
   def get_database_tracks
-  	most_recent_database_tracks = TrackData.last(5)
-  	render :json => most_recent_database_tracks
+    last_offset = 5
+    reverse_offset = TrackData.count - last_offset
+  	requested_tracks = TrackData.limit(5).offset(reverse_offset)
+  	render :json => requested_tracks
   end
 
   #test controllers are below
